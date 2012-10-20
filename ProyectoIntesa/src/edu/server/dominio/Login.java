3b0945aff6bd0cf1fb082ca95528b2bbc7de2989
pablo <pablodchiotti@hotmail.com>
2012-10-20 19:16:33 -0300

    metodo getUsuario clase Login

--- ProyectoIntesa/src/edu/server/dominio/Login.java
+++ ProyectoIntesa/src/edu/server/dominio/Login.java
@@ -12,6 +12,27 @@ public class Login {
 	public Login() {
 
 	}
+	
+public Usuario getUsuario(String nombreUsuario, String pass){
+		
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		
+		try{
+			session.beginTransaction();
+			
+			Usuario usr = new Usuario();
+			
+			usr = (Usuario) session.createQuery("from usuario where usuario= '"+nombreUsuario+"' and contrasenia='"+pass+"'").list().get(0);
+			
+			if(usr != null)
+				return usr;		
+			
+		} catch (HibernateException he){
+			
+			return null;
+		}
+		return null;
+	}
 
 	
 }
