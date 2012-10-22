2b1199a956c6513699275242f2654bee48038bde
pablo <pablodchiotti@hotmail.com>
2012-10-22 11:56:43 -0300

    2012-10-22 test login usuario

--- ProyectoIntesa/src/edu/server/dominio/Login.java
+++ ProyectoIntesa/src/edu/server/dominio/Login.java
@@ -6,6 +6,7 @@ import org.hibernate.Session;
 import edu.server.repositorio.Empleado;
 import edu.server.repositorio.Usuario;
 import edu.server.util.HibernateUtil;
+import edu.shared.DTO.UsuarioDTO;
 
 public class Login {
 
@@ -13,26 +14,55 @@ public class Login {
 
 	}
 	
-public Usuario getUsuario(String nombreUsuario, String pass){
+	/**
+	 * 
+	 * @param nombreUsuario
+	 * @param pass
+	 * @return
+	 */
+	
+public UsuarioDTO getUsuario(String nombreUsuario, String pass){
 		
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 		
 		try{
 			session.beginTransaction();
 			
-			Usuario usr = new Usuario();
+			Usuario usr= new Usuario();
+			UsuarioDTO respuesta  = new UsuarioDTO();
 			
-			usr = (Usuario) session.createQuery("from usuario where usuario= '"+nombreUsuario+"' and contrasenia='"+pass+"'").list().get(0);
+			System.out.println("from Usuario where usuario like '"+nombreUsuario+"' and contrasenia like '"+pass+"'");
+			usr = (Usuario) session.createQuery("from Usuario where usuario like '"+nombreUsuario+"' and contrasenia like '"+pass+"'").list().get(0);
 			
 			if(usr != null)
-				return usr;		
+			{
+				Empleado emp = new Empleado();
+				emp = (Empleado) session.get(Empleado.class, usr.getEmpleado().getIdEmpleado());
+				respuesta.setNombre(emp.getNombre()+" "+emp.getApellido());
+				respuesta.setRol(usr.getRol());
+				
+				session.close();
+				return respuesta;
+			}
 			
-		} catch (HibernateException he){
-			
+		} catch (HibernateException he){	
+			session.close();
 			return null;
 		}
+		session.close();
 		return null;
-	}
+	}	
 
-	
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- ProyectoIntesa/test/edu/server/dominio/LoginTest.java
+++ ProyectoIntesa/test/edu/server/dominio/LoginTest.java
@@ -1,6 +1,7 @@
 package edu.server.dominio;
 
 import static org.junit.Assert.*;
+import junit.framework.Assert;
 
 import org.hibernate.Session;
 import org.junit.After;
@@ -10,7 +11,9 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import edu.server.repositorio.Empleado;
+import edu.server.repositorio.Usuario;
 import edu.server.util.HibernateUtil;
+import edu.shared.DTO.UsuarioDTO;
 
 public class LoginTest {
 
@@ -24,16 +27,25 @@ public class LoginTest {
 		empleadoPrueba.setNroLegajo(15);
 		empleadoPrueba.setPuesto("sistema");
 		
+		Usuario usuarioPrueba = new Usuario();
+		usuarioPrueba.setEmpleado(empleadoPrueba);
+		usuarioPrueba.setUsuario("arnaldo");
+		usuarioPrueba.setContrasenia("tibaldo");
+		usuarioPrueba.setRol("seo");
+		
 		Session sesion = HibernateUtil.getSessionFactory().getCurrentSession();
 		sesion.beginTransaction();
 		sesion.save(empleadoPrueba);
+		sesion.save(usuarioPrueba);
 		sesion.getTransaction().commit();
+		
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 		Session sesion = HibernateUtil.getSessionFactory().getCurrentSession();
 		sesion.beginTransaction();
+		sesion.createQuery("delete from Usuario").executeUpdate();
 		sesion.createQuery("delete from Empleado").executeUpdate();
 		sesion.getTransaction().commit();
 	}
@@ -46,8 +58,15 @@ public class LoginTest {
 	@After
 	public void tearDown() throws Exception {
 	}
-
-
+	
+	@Test
+	public void getUsuarioTest()
+	{
+		UsuarioDTO respuesta = pruebaLogin.getUsuario("arnaldo", "tibaldo");
+		assertEquals("arnaldo tibaldo", respuesta.nombre);
+		assertEquals("seo", respuesta.rol);
+	}
+	
 
 
 }
